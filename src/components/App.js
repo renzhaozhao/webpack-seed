3d83e931fde4192b3bff39c2bbda4c46fb21b161
renzhao <renzhao@vpgame.cn>
2017-05-22 14:53:33 +0800

    添加class静态类

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+
+  state = {
+    number: 1
+  }
+
   componentWillMount() {
     console.log('111')
   }
+
+  add = () => {
+    this.setState(prevState => ({
+      number: prevState.number + 1
+    }))
+  }
+
   render() {
     return (
       <div>
         <img src={require('../assets/avatar.png')} style={{ width: 100 }} alt="" />
         <img src={require('../assets/test.jpg')} alt="" />
+        {this.state.number}
+        <button onClick={this.add}>++</button>
       </div>
     )
   }
